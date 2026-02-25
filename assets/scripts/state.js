6a2a291ae08ddd36d6e3dce8b959d6864ceb04b8
jasseramir <jasser.amir.work@gmail.com>
2026-02-25 20:34:20 +0200

    Create state.js

---
+++ assets/scripts/state.js
@@ -0,0 +1,22 @@
+const addSubject = document.getElementById('addSubject'),
+      formContainer = document.getElementById('formContainer'),
+      form = document.getElementById('form'),
+      subjectName = document.getElementById('subjectName'),
+      studentGrade = document.getElementById('studentGrade'),
+      difficulty = document.getElementById('difficulty'),
+      studyingHours = document.getElementById('studyingHours'),
+      examSoon = document.getElementById('examSoon'),
+      addForm = document.getElementById('addForm'),
+      reset = document.getElementById('reset'),
+      formClose = document.getElementById('formClose');
+
+const createTable = document.getElementById('createTable');
+const scheduleBox = document.getElementById('schedule');
+
+const output = document.getElementById('subjects');
+const stored = localStorage.getItem('subjects');
+
+let subjects = stored ? JSON.parse(stored) : [];
+let idCounter = localStorage.getItem('idCount') ? Number(localStorage.getItem('idCount')) : 0;
+
+let scheduleVisible = false;
